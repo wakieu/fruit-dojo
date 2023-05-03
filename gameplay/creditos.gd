5173b36d69409331b71c57756c7ee743f2ea6f68
viniw <viniwte@gmail.com>
2023-05-03 18:09:16 -0300

    fruit dojo game

---
+++ gameplay/creditos.gd
@@ -0,0 +1,35 @@
+extends Resource
+
+
+# Este arquivo vai ser usado diretamente nos créditos do jogo! Dê crédito a todas as pessoaas que
+# participaram da criação do jogo e a todos os recursos externos utilizados. Lembre que a fonte que
+# será utilizada não suporte caracteres latinos como ~ ou ´
+
+
+# Título do seu jogo
+var title_pt = "Fwuit Samuwai"
+var title_en = "Fwuit Samuwai"
+
+# Detalhes dos créditos do seu jogo
+var credits_pt = """
+Criado por: wakieu
+
+https://opengameart.org/node/21050
+https://opengameart.org/content/cc0-food-icons
+https://opengameart.org/content/weapon-slash-effect
+https://opengameart.org/content/samurai
+https://opengameart.org/content/swish-bamboo-stick-weapon-swhoshes
+https://opengameart.org/content/swishes-sound-pack
+https://opengameart.org/content/error
+"""
+var credits_en = """
+Made by: wakieu
+
+https://opengameart.org/node/21050
+https://opengameart.org/content/cc0-food-icons
+https://opengameart.org/content/weapon-slash-effect
+https://opengameart.org/content/samurai
+https://opengameart.org/content/swish-bamboo-stick-weapon-swhoshes
+https://opengameart.org/content/swishes-sound-pack
+https://opengameart.org/content/error
+"""
